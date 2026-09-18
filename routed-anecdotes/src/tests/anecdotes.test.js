36534269ceb0ede9a4f37fd99ae38f2733d5d274
mediatuottajavantaa <jyrgenhuhtala@gmail.com>
2026-09-18 14:37:25 +0300

    servicelle testien kirjoittaminen aloitetettu

---
+++ routed-anecdotes/src/tests/anecdotes.test.js
@@ -0,0 +1,10 @@
+import {describe, expect, test, beforeEach} from 'vitest'
+
+describe('anecdoteService', () => {
+    beforeEach(() => {
+        global.fetch = vi.fn()
+    })
+    describe('getAll', () => {
+        test('should ', () => {})
+    })
+})
